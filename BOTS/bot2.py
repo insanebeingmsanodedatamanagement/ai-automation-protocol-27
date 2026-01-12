57fe5d558638797b50bcc6b4623570b3519ed2f3
insanebeingmsanodedatamanagement <msanodedata@gmail.com>
2026-01-12 21:57:48 +0530

    Update bot2.py

--- BOTS/bot2.py
+++ BOTS/bot2.py
@@ -4,7 +4,6 @@ import os
 import csv
 import time
 import threading
-from dotenv import load_dotenv
 from aiohttp import web
 
 
@@ -5312,3 +5311,4 @@ if __name__ == "__main__":
         asyncio.run(startup())
     except (KeyboardInterrupt, SystemExit):
         print("🛑 Command Hub Stopped Safely")
+
